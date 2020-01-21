2304031bcdafb85f2378fb5911ca3cd60de49c4f
Souen Boniface <souenboniface@gmail.com>
2020-01-21 09:55:59 +0100

    feat(fields): take "blank" option of TemplateField into account

--- django_reactive/fields.py
+++ django_reactive/fields.py
@@ -30,12 +30,20 @@ class TemplateField(CharField):
         kwargs.setdefault('default', dict)
         self.templates = templates
         if self.templates:
-            kwargs['choices'] = [(name, label) for (name, label, schema) in templates]
+            choices = []
+            if kwargs.get("blank"):
+                choices.append(("", "------"))
+            choices.extend([(name, label) for (name, label, schema) in templates])
+            kwargs['choices'] = choices
             self.templates = {name: schema for (name, label, schema) in templates}
         super(TemplateField, self).__init__(**kwargs)
 
     def formfield(self, **kwargs):
+        defaults = {
+            'required': not self.blank,
+        }
+        defaults.update(**kwargs)
         return TemplateFormField(
             widget=TemplateFormWidget(choices=self.choices, templates=self.templates),
-            **kwargs
+            **defaults
         )
