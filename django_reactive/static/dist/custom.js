7277ac0f2a7279cd5708a2f7228cede7e030960d
Thomas C <thomas.carbillet@gmail.com>
2020-05-19 16:59:27 +0200

    wip

--- django_reactive/static/dist/custom.js
+++ django_reactive/static/dist/custom.js
@@ -1,14 +1,14 @@
-tinymce.init({
-  selector: 'textarea',
-  height: 500,
-  menubar: false,
-  plugins: [
-    'advlist autolink lists link charmap preview',
-    'searchreplace visualblocks nonbreaking',
-    'insertdatetime media table paste code wordcount code'
-  ],
-  toolbar: 'undo redo | link | bold italic underline | blockquote | subscript superscript | bullist | nonbreaking | charmap | code',
-  content_css: '//www.tiny.cloud/css/codepen.min.css',
-  default_link_target: "_blank",
-  nonbreaking_wrap: false,
-});
+// tinymce.init({
+//   selector: 'textarea',
+//   height: 500,
+//   menubar: false,
+//   plugins: [
+//     'advlist autolink lists link charmap preview',
+//     'searchreplace visualblocks nonbreaking',
+//     'insertdatetime media table paste code wordcount code'
+//   ],
+//   toolbar: 'undo redo | link | bold italic underline | blockquote | subscript superscript | bullist | nonbreaking | charmap | code',
+//   content_css: '//www.tiny.cloud/css/codepen.min.css',
+//   default_link_target: "_blank",
+//   nonbreaking_wrap: false,
+// });
