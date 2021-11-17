a92c46481702bc1a1884e16fefa6889de1157987
Souen Boniface <souenboniface@gmail.com>
2021-11-17 11:10:34 +0100

    feat(views): enable specifying model and field_name from urls

--- django_reactive/views.py
+++ django_reactive/views.py
@@ -7,21 +7,31 @@ from django.http import HttpResponse, Http404
 
 
 class URIImageAsFileView(View):
-    model = None
-    field = None
+    model_class = None
+    field_name = None
 
-    def get(self, request, pk, pointer):
+    def get(self, request, pk, pointer, model_class=None, field_name=None):
         """
         :param request:
         :param pk: the pk of the instance
-        :param pointer: the json pointer of the image. In the form "image", "people/image" or
-        "people/0/image"
-        :return: HttpResonse with the image
+        :param pointer: the json pointer of the image. Ex: "people/image", "people/0/image", etc.
+        :param model_class: the model that holds the JSON field
+        :param field_name: the JSON-field name
+        :return: HttpResponse with the image
 
         Base view to serve an URI-stored (in a JSON field) image as a file.
+
+        Example usage in urls.py:
+
+            re_path(
+                r"some/path/(?P<pk>\d+)/(?P<pointer>.+)/",
+                URIImageAsFileView.as_view(),
+                kwargs={"model_class": MyModel, "field_name": "my_json_field"},
+            ),
+
         """
-        obj = get_object_or_404(self.model, pk=pk)
-        data = getattr(obj, self.field)
+        obj = get_object_or_404(model_class or self.model_class, pk=pk)
+        data = getattr(obj, field_name or self.field_name)
         pointer = "/{}".format(pointer)
         try:
             data = resolve_pointer(data, pointer)
